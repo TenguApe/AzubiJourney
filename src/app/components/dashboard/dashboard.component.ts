2d9cea51acaec0f053803233e74a810d4351ab76
Dominik Talar <dominik.talar@conplement.de>
2024-03-15 08:13:26 +0100

    wip

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {Field} from "../../field";
 import {FieldService} from "../../fieldService";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   fields: Field[] = [];
-  btnRight: any;
 
   constructor(private fieldService: FieldService) {
   }
 
   ngOnInit() {
     this.getFields();
+    console.log(this.fields)
   }
 
   private getFields() {
